45cb2ca309d2de45388ef773aac6cf944fd6754a
Bhaskar Purohit <bhaskarpurohit22@gmail.com>
2023-09-05 23:46:36 +0530

    new

--- week3.1/index.js
+++ week3.1/index.js
@@ -1,3 +1,14 @@
+const { error } = require('console')
 const fs = require('fs')
 
-fs.readFile('file.txt','utf8')
+const callbackFn = (err, data)=>{
+    if(err){
+        error("not available")
+    }
+    else{
+        return data
+    }
+}
+
+
+fs.readFile('file.txt','utf8',callbackFn)
